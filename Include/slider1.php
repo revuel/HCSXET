34c468806968be8d7ea84b50f6737435022216b6
revuel <revuel22@hotmail.com>
2015-03-09 23:14:24 +0100

    1.02

--- Include/slider1.php
+++ Include/slider1.php
@@ -1,29 +1,45 @@
-<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+<div id="carousel-example-generic" class="carousel slide text-center" data-ride="carousel">
   <!-- Indicators -->
-  <ol class="carousel-indicators">
-    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-	<li data-target="#carousel-example-generic" data-slide-to="3"></li>
-	<li data-target="#carousel-example-generic" data-slide-to="4"></li>
-  </ol>
+	<ol class="carousel-indicators">
+		<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+		<li data-target="#carousel-example-generic" data-slide-to="1"></li>
+		<li data-target="#carousel-example-generic" data-slide-to="2"></li>
+		<li data-target="#carousel-example-generic" data-slide-to="3"></li>
+		<li data-target="#carousel-example-generic" data-slide-to="4"></li>
+	</ol>
 
   <!-- Wrapper for slides -->
   <div class="carousel-inner" role="listbox">
     <div class="item active">
-      <img src="Pics/moldeprincipios.png" alt="...">
-      <div class="carousel-caption">
-      Principio 1
-      </div>
+		<img src="Pics/Principio+1.png" alt="...">
+		<div class="carousel-caption">
+			<h3>...</h3>
+			<p>...</p>
+		</div>
     </div>
     <div class="item">
-      <img src="Pics/moldepreguntas.png" alt="...">
-      <div class="carousel-caption">
-	   Principio 2
-      </div>
+		<img src="Pics/Principio+2.png" alt="...">
+		<div class="carousel-caption">
+			<h3>...</h3>
+			<p>...</p>
+		</div>
     </div>
 	<div class="item">
-		<img src="Pics/logoufv.png" alt="...">
+		<img src="Pics/Principio+3.png" alt="...">
+		<div class="carousel-caption">
+			<h3>...</h3>
+			<p>...</p>
+		</div>
+	</div>
+	<div class="item">
+		<img src="Pics/Principio+4.png" alt="...">
+		<div class="carousel-caption">
+			<h3>...</h3>
+			<p>...</p>
+		</div>
+	</div>
+	<div class="item">
+		<img src="Pics/Principio+5.png" alt="...">
 		<div class="carousel-caption">
 			<h3>...</h3>
 			<p>...</p>
@@ -41,43 +57,4 @@
     <span class="sr-only">Next</span>
   </a>
 </div>
-
-<!--<div id="myCarousel" class="carousel slide" data-ride="carousel">
-    <!-- Indicators 
-    <ol class="carousel-indicators">
-      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-      <li data-target="#myCarousel" data-slide-to="1"></li>
-      <li data-target="#myCarousel" data-slide-to="2"></li>
-      <li data-target="#myCarousel" data-slide-to="3"></li>
-    </ol>
-
-    <!-- Wrapper for slides 
-    <div class="carousel-inner" role="listbox">
-      <div class="item active">
-        <img src="Pics/moldeprincipios.png" alt="Chania" width="460" height="345">
-      </div>
-
-      <div class="item">
-        <img src="Pics/moldepreguntas.png" alt="Chania" width="460" height="345">
-      </div>
-    
-      <div class="item">
-        <img src="Pics/moldeprincipios.png" alt="Flower" width="460" height="345">
-      </div>
-
-      <div class="item">
-        <img src="Pics/moldepreguntas.png" alt="Flower" width="460" height="345">
-      </div>
-    </div>
-
-    <!-- Left and right controls 
-    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-      <span class="sr-only">Next</span>
-    </a>
-  </div>
-</div> -->
+<p class = "text-center">Principios recopilados del libro "Collected Esays of Human Centered Computing"</p>
